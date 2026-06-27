1125ea138ed8536e58c122b8395c4d64bff73d38
8bitmcu <agashka2@gmail.com>
2026-06-27 18:14:00 -0300

    github actions

--- modules/manifest.py
+++ modules/manifest.py
@@ -1,4 +1,7 @@
 import os
 
-SCRIPTS_DIR = pwd() + "/scripts"
-freeze(SCRIPTS_DIR)
+# Read the path passed by the build system.
+# Fall back to your local path if the environment variable isn't found.
+base_path = os.getenv("MPY_USER_MODULES_DIR", "/opt/all_modules")
+
+freeze(base_path + "/scripts")
