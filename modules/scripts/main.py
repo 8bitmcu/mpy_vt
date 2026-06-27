65ca372d653ed9dc0cecc34accdfa9b62b1b11eb
8bitmcu <agashka2@gmail.com>
2026-06-27 19:41:41 -0300

    cicd test

--- modules/scripts/main.py
+++ modules/scripts/main.py
@@ -11,7 +11,6 @@ import network
 import sys
 import time
 import telnet
-from secrets import secrets
 import st7789
 import time
 import status
@@ -181,7 +180,7 @@ def connect_wlan():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
-        wlan.connect(secrets['ssid'], secrets['pw'])
+        #wlan.connect(secrets['ssid'], secrets['pw'])
         while not wlan.isconnected():
             sys.stdout.write(f"{YELLOW}.{CLR}") 
             time.sleep(1)
