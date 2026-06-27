5b6ab6ba0dbb6507fc5c3f7f5844800594d5c69f
8bitmcu <agashka2@gmail.com>
2026-06-27 14:51:01 -0300

    github actions

--- boards/LILYGO_T_DECK/mpconfigboard.cmake
+++ boards/LILYGO_T_DECK/mpconfigboard.cmake
@@ -1,12 +1,12 @@
 set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
-    ${CMAKE_CURRENT_LIST_DIR}/../../sdkconfig.base
-    ${CMAKE_CURRENT_LIST_DIR}/../../sdkconfig.ble
-    ${CMAKE_CURRENT_LIST_DIR}/../../sdkconfig.spiram_sx
+    ${CMAKE_CURRENT_LIST_DIR}/../sdkconfig.base
+    ${CMAKE_CURRENT_LIST_DIR}/../sdkconfig.ble
+    ${CMAKE_CURRENT_LIST_DIR}/../sdkconfig.spiram_quad
     ${CMAKE_CURRENT_LIST_DIR}/sdkconfig.board
-    ${CMAKE_CURRENT_LIST_DIR}/../../sdkconfig.240mhz
-    ${CMAKE_CURRENT_LIST_DIR}/../../sdkconfig.spiram_oct
+    ${CMAKE_CURRENT_LIST_DIR}/../sdkconfig.240mhz
+    ${CMAKE_CURRENT_LIST_DIR}/../sdkconfig.spiram_oct
 )
 
 
