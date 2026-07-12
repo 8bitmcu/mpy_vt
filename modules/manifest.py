aec836822d75987c619eebb9b15a4c7dc0c2f862
8bitmcu <agashka2@gmail.com>
2026-07-12 02:31:03 -0300

    mpy env

--- modules/manifest.py
+++ modules/manifest.py
@@ -2,11 +2,9 @@ import os
 
 # Read the path passed by the build system.
 # Fall back to your local path if the environment variable isn't found.
-base_path = os.getenv("MPY_USER_MODULES_DIR", "/opt/all_modules")
-mpy_dir = os.getenv("MPY_DIR", "/opt/micropython")
 
-
+mpy_dir = os.getenv("MPY_USER_DIR", "/opt/micropython")
 include(mpy_dir + "/extmod/asyncio/manifest.py")
 
-
+base_path = os.getenv("MPY_USER_MODULES_DIR", "/opt/all_modules")
 freeze(base_path + "/scripts")
